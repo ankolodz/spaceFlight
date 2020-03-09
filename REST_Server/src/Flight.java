c6de252b02d76cdaeaefae037a49d8787cf55895
ankolodz <49743845+ankolodz@users.noreply.github.com>
2020-03-09 12:17:57 +0100

    Added addedTourist/Flights and getAllTourist/Flights

--- REST_Server/src/Flight.java
+++ REST_Server/src/Flight.java
@@ -1,3 +1,6 @@
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 import javax.persistence.*;
 import java.util.Date;
 import java.util.LinkedList;
@@ -18,5 +21,30 @@ public class Flight {
     @ManyToMany(cascade = CascadeType.PERSIST)
     private List<Tourist> tourist = new LinkedList<Tourist>();
 
+    public Flight() {
+    }
+    public Flight(Date departue, Date arrival, int numberOfSeat, int numberOfTourist, double price) {
+        this.departue = departue;
+        this.arrival = arrival;
+        this.numberOfSeat = numberOfSeat;
+        this.numberOfTourist = numberOfTourist;
+        this.price = price;
+    }
 
+    public static void newFlight (Date departue, Date arrival, int numberOfSeat, int numberOfTourist, double price){
+        Session session = Main.getSession();
+        Transaction tx = session.beginTransaction();
+        Flight flight = new Flight( departue,  arrival,  numberOfSeat,  numberOfTourist,  price);
+        session.save(flight);
+        tx.commit();
+        session.close();
+    }
+    public static List<Flight> getAllFlight (){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("myDatabaseConfig");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction etx = em.getTransaction();
+        etx.begin();
+        return (List<Flight>) em.createQuery(
+                "SELECT f FROM Flight f").getResultList();
+    }
 }
--- REST_Server/src/Tourist.java
+++ REST_Server/src/Tourist.java
@@ -1,3 +1,6 @@
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 import javax.persistence.*;
 import java.util.Date;
 import java.util.LinkedList;
@@ -19,4 +22,33 @@ public class Tourist {
     @ManyToMany(mappedBy = "tourist",cascade = CascadeType.PERSIST)
     private List<Flight> flights = new LinkedList<Flight>();
 
+    public Tourist(){
+
+    }
+    public Tourist(String name, String sourname, Character sex, String country, String notes, Date birthDate) {
+        this.name = name;
+        this.sourname = sourname;
+        this.sex = sex;
+        this.country = country;
+        this.notes = notes;
+        this.birthDate = birthDate;
+    }
+
+    public static void newTourist(String name, String sourname, Character sex, String country, String notes, Date birthDate) {
+        Session session = Main.getSession();
+        Transaction tx = session.beginTransaction();
+        Tourist tourist = new Tourist(name, sourname, sex, country, notes, birthDate);
+        session.save(tourist);
+        tx.commit();
+        session.close();
+    }
+
+    public static List<Tourist> getAllTourist (){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("myDatabaseConfig");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction etx = em.getTransaction();
+        etx.begin();
+        return (List<Tourist>) em.createQuery(
+                "SELECT t FROM Tourist t").getResultList();
+    }
 }
